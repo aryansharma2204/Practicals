c7b47ae4cce2ca20335bdbccb3dda4d91b3cd061
Aryan Sharma <aryansde2204@gmail.com>
2023-07-01 07:38:33 +0530

    Create 19.c

---
+++ C Practicals/2nd Sem/19.c
@@ -0,0 +1,79 @@
+//write a program to implement recursive algorithm for BST traversals- inorder, preorder, postorder
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node* insert(Node* root, int data) {
+    if (root == NULL) {
+        root = createNode(data);
+    } else if (data <= root->data) {
+        root->left = insert(root->left, data);
+    } else {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+void inorderTraversal(Node* root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+void preorderTraversal(Node* root) {
+    if (root != NULL) {
+        printf("%d ", root->data);
+        preorderTraversal(root->left);
+        preorderTraversal(root->right);
+    }
+}
+
+void postorderTraversal(Node* root) {
+    if (root != NULL) {
+        postorderTraversal(root->left);
+        postorderTraversal(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+    printf("Aryan Sharma\n");
+    Node* root = NULL;
+
+    root = insert(root, 50);
+    root = insert(root, 30);
+    root = insert(root, 20);
+    root = insert(root, 40);
+    root = insert(root, 70);
+    root = insert(root, 60);
+    root = insert(root, 80);
+
+    printf("Inorder traversal of the BST: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    printf("Preorder traversal of the BST: ");
+    preorderTraversal(root);
+    printf("\n");
+
+    printf("Postorder traversal of the BST: ");
+    postorderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
