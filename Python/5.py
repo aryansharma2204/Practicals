35c03109054d11280e05118d8cd35981b891947e
Aryan Sharma <aryansde2204@gmail.com>
2023-10-15 15:25:55 +0530

    Create 5.py

---
+++ Python/5.py
@@ -0,0 +1,12 @@
+# wap to find the smallest and greatest number in the list.
+
+# Input a list of numbers (you can modify this list)
+numbers = [5, 10, 2, 8, 15, 1, 20]
+
+# Find the smallest and greatest numbers in the list
+smallest = min(numbers)
+greatest = max(numbers)
+
+# Display the results
+print(f"The smallest number in the list is: {smallest}")
+print(f"The greatest number in the list is: {greatest}")
